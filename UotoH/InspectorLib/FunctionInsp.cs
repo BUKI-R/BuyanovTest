03f0ff94f696c3559e6bca06e33600b9e8068924
BUKI <buyunov2005@gmail.com>
2024-11-29 14:54:36 +0900

    BuyanovTest

---
+++ UotoH/InspectorLib/FunctionInsp.cs
@@ -0,0 +1,45 @@
+﻿namespace InspectorLib
+{
+    public class FunctionInsp
+    {
+        // создание списка представленого организацией
+        private static string AutoInspect = "Автоинспекция г. Чита";
+        private static string GlavnInspector = "Васильев В.И.";
+        private static List<string> sotrudnic = new List<string>(){"Иванов И.И.", "Зиронов Т.А.", "Миронов А.В.", "Васильев В.И."};
+        
+        // создание методов стого по ТЗ
+        public static string GetInspector() => GlavnInspector;
+
+        public static string GetCarInspection() => AutoInspect;
+
+        public static bool SetInspector(string fullname)
+        {
+            if (sotrudnic.Contains(fullname))
+            {
+                GlavnInspector = fullname;
+                return true;
+            }
+            return false;
+        }
+
+        public static string GenerateNumber(string symbol, int code = 75)
+        {
+            Random number_rand= new Random();
+            int number = number_rand.Next(1000, 10000);
+            return $"{symbol.ToUpper()}{number}_{code}";
+        }
+
+        public static List<string> GetWorkers() => sotrudnic;
+
+        public static bool AddWorker(string fullname)
+        {
+            if (!sotrudnic.Contains(fullname))
+            {
+                sotrudnic.Add(fullname);
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
---
+++ UotoH/UotoH/Program.cs
@@ -0,0 +1,23 @@
+﻿using InspectorLib;
+// ввыод изначального инспектора
+Console.WriteLine($"Название автоинспекции: {FunctionInsp.GetCarInspection()}");
+Console.WriteLine($"Главный инспектор: {FunctionInsp.GetInspector()}");
+Console.WriteLine($"Список сотрудников: {string.Join(", ", FunctionInsp.GetWorkers())}");
+
+// замена инспектора из существующего списка
+Console.Write($"\r\nВыберите сотрудника из списка: ");
+string OLDhumen = Console.ReadLine();
+bool proverc = FunctionInsp.SetInspector(OLDhumen);
+Console.WriteLine($"Изменение главного инспектора на {OLDhumen}: {proverc}");
+Console.WriteLine($"Главный инспектор: {FunctionInsp.GetInspector()}");
+
+// проверка генерация номера автомобиля
+Console.Write($"\r\nВведите буквы для номера: ");
+string newNumber = FunctionInsp.GenerateNumber(Console.ReadLine());
+Console.WriteLine($"Сгенерированный госномер: {newNumber}");
+
+// проверка добавление нового сотрудника
+Console.Write($"\r\nВведите имя нового сотрудника: ");
+string Newhumen = Console.ReadLine();
+Console.WriteLine($"Добавление сотрудника {Newhumen}");
+Console.WriteLine("Список сотрудников: " + string.Join(", ", FunctionInsp.GetWorkers()));
